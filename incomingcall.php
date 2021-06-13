3f11b3ecf0a956daa76daa7976f44e51ac4e0907
Festus Okonye <67989072+festuso101@users.noreply.github.com>
2021-06-13 23:58:34 +0100

    Update incomingcall.php

--- incomingcall.php
+++ incomingcall.php
@@ -1,27 +1,12 @@
 <?php
 
 require './twilio-php/src/Twilio/autoload.php';
-// use Twilio\Rest\Client;
+
 use Twilio\TwiML\VoiceResponse;
 
 
-$sid = "ACf8182bcaa986cf3337692ebaa8d9adc3"; // Your Account SID from www.twilio.com/console
-$token = "deb9fd74093a5f05fb0ecc56aa83b264"; // Your Auth Token from www.twilio.com/console
-
-// $client = new Twilio\Rest\Client($sid, $token);
-
-// $twilio = new Client($sid, $token);
-
-// $call = $twilio->calls
-//               ->create("+18064783311", // to
-//                         "+14439880198", // from
-//                         [
-//                             "twiml" => "<Response><Say>Hello, What's the update!</Say></Response>"
-//                         ]
-//               );
-
-//print($call->sid);
-
+$sid = "SID"; // Your Account SID from www.twilio.com/console
+$token = "AUTH TOKEN"; // Your Auth Token from www.twilio.com/console
 
 // Start our TwiML response
 $response = new VoiceResponse;
@@ -34,7 +19,5 @@ $response->say(
 
 
 echo $response;
-
-//echo "Call Sent";
 
 ?>
