bd6f4f7fe2ee8eae54360342b7846d2e469d97be
Festus Okonye <67989072+festuso101@users.noreply.github.com>
2021-06-13 23:59:44 +0100

    Update outgoingcall.php

--- outgoingcall.php
+++ outgoingcall.php
@@ -5,18 +5,17 @@ use Twilio\Rest\Client;
 
 
 
-$sid = "ACf8182bcaa986cf3337692ebaa8d9adc3"; // Your Account SID from www.twilio.com/console
-$token = "deb9fd74093a5f05fb0ecc56aa83b264"; // Your Auth Token from www.twilio.com/console
-
+$sid = "SID"; // Your Account SID from www.twilio.com/console
+$token = "AUTH TOKEN"; // Your Auth Token from www.twilio.com/console
 $client = new Twilio\Rest\Client($sid, $token);
 
 $twilio = new Client($sid, $token);
 
 $call = $twilio->calls
-              ->create("+18064783311", // to
-                        "+14439880198", // from
+              ->create("+1000000000", // to
+                        "+100000000000", // from
                         [
-                            "twiml" => "<Response><Say>Hello, What's the update!</Say></Response>"
+                            "twiml" => "<Response><Say>Hello, World!</Say></Response>"
                         ]
               );
 
