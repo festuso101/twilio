275bd52b97b3a49fab61f1ac4466d20e96498937
Festus Okonye <67989072+festuso101@users.noreply.github.com>
2021-06-14 00:01:43 +0100

    Update text.php

--- text.php
+++ text.php
@@ -4,19 +4,19 @@
 require './twilio-php/src/Twilio/autoload.php';
 use Twilio\Rest\Client;
 
-$sid = "ACf8182bcaa986cf3337692ebaa8d9adc3"; // Your Account SID from www.twilio.com/console
-$token = "deb9fd74093a5f05fb0ecc56aa83b264"; // Your Auth Token from www.twilio.com/console
+$sid = "SID"; // Your Account SID from www.twilio.com/console
+$token = "AUTH TOKEN"; // Your Auth Token from www.twilio.com/console
 
 $client = new Twilio\Rest\Client($sid, $token);
 $message = $client->messages->create(
-  '18064783311', // Text this number
+  '1000000000000000000', // Text this number
   [
-    'from' => '14439880198', // From a valid Twilio number
+    'from' => '10000000000000', // From a valid Twilio number
     'body' => 'This is just to prove that this actually works.'
   ]
 );
 
-//print $message->sid;
+print $message->sid;
 
 echo "Message Sent";
 
